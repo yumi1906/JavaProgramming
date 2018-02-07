99917c0611872b252f8fb5efc11719977cf23259
yumi <yuwasa60@gmail.com>
2018-02-08 00:26:11 +0900

    Delete Example2.java

--- Chapter01/1.8/Example2.java
+++
@@ -1,8 +0,0 @@
-//練習問題１．８
-//初めてのJavaアプリケーション
-class Example2{
-  public static void main(String args[]){
-    System.out.println("This is on the first line.");
-    System.out.println("This is on the second line.");
-  }
-}
