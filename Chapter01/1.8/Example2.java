b822f9fa3aabfe2f2cfcc8bb5f22491a9f90777a
yumi <yuwasa60@gmail.com>
2018-02-08 00:20:11 +0900

    Create Example2.java

---
+++ Chapter01/1.8/Example2.java
@@ -0,0 +1,8 @@
+//練習問題１．８
+//初めてのJavaアプリケーション
+class Example2{
+  public static void main(String args[]){
+    System.out.println("This is on the first line.");
+    System.out.println("This is on the second line.");
+  }
+}
